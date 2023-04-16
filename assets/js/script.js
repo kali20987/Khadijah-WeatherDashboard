417c8f9fadbbc593598a6cc3b7e7180f4add26ba
kali19 <khadijah.syed.ali@gmail.com>
2023-04-16 19:35:24 -0400

    attempt with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
-console.log("hey");
+var searchInput = document.getElementById("search-form");
+var apiKey= "60779efd43f7b8c58d8938853d39d5e2";
+var searchBtn = document.getElementById("search-btn");
+var cityName = "";
 
-fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&60779efd43f7b8c58d8938853d39d5e2={API key}")
-    .then(function(response) {
-        return response.json();
+console.log(searchInput);
+
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    var userInput = searchInput.value.toLowerCase();
+    fetch("api.openweathermap.org/data/2.5/forecast?" + cityName + "&appid=" + apiKey + "&units=imperial" + userInput)
+        .then(function(response) {
+            return response.json();
     })
-    .then(function(data) {
+        .then(function(data) {
         console.log(data);
     })
+})
