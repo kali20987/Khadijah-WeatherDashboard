87e9950d9dad5fbf037a2f71236342f6c9ccdaf7
kali19 <khadijah.syed.ali@gmail.com>
2023-04-17 20:14:40 -0400

    got data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,48 @@
-var searchInput = document.getElementById("search-form");
-var apiKey= "60779efd43f7b8c58d8938853d39d5e2";
+var searchInput = document.getElementById("search-input");
+const apiKey = "60779efd43f7b8c58d8938853d39d5e2";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?";
 var searchBtn = document.getElementById("search-btn");
-var cityName = "";
+var data = '';
 
 console.log(searchInput);
 
-searchBtn.addEventListener('click', function(event){
+showForecast();
+
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var userInput = searchInput.value.toLowerCase();
-    fetch("api.openweathermap.org/data/2.5/forecast?" + cityName + "&appid=" + apiKey + "&units=imperial" + userInput)
-        .then(function(response) {
+
+    // var lat = '';
+    // var lon = '';
+    //    fetch("https://api.openweathermap.org/data/2.5/forecast?"
+    // fetch(forecastURL + userInput + "&appid=" + apiKey + "&units=imperial")
+    //     .then(function (response) {
+    //         return response.json();
+    //     })
+    //     .then(function (data) {
+    //         console.log(data);
+    //         lat = data.city.coord.lat;
+    //         lon = data.city.coord.lon;
+    //         console.log('lat = ' + lat);
+    //         console.log('lon = ' + lon);
+    //     })
+
+    // await (lat!='');
+    // await (lon!='');
+
+    // fetch(weatherURL + "lat=" + lat + "&lon=" + lon + "&appid=" + apiKey +
+    //    "&units=imperial")
+    fetch(forecastURL + userInput + "&appid=" + apiKey + "&units=imperial")
+        .then(function (response) {
             return response.json();
-    })
-        .then(function(data) {
-        console.log(data);
-    })
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
 })
+
+function showForecast(){
+
+}
